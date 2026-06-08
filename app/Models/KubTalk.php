c041d8dc41537650b1366674183daa8af345aabd
Akbarnama Tangguh Dipantara <74764481+DipantaraTangguh@users.noreply.github.com>
2026-06-08 15:41:14 +0700

    feat: add PHPDoc property annotations to KubTalk model for improved type hinting

--- app/Models/KubTalk.php
+++ app/Models/KubTalk.php
@@ -5,6 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string|null $image
+ * @property array|null $images
+ * @property string $title
+ * @property string|null $description
+ * @property string|null $company_name
+ * @property string|null $company_logo
+ * @property string|null $speaker_name
+ * @property string|null $speaker_title
+ * @property \Carbon\Carbon|null $event_date
+ * @property int $sort_order
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ */
 class KubTalk extends Model
 {
     protected $fillable = [
